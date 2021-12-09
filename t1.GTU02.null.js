c817b254c2f30fda37ec85bf5a50f394560eb2b7
Elen <elen.ghazaryan6@gmail.com>
2021-12-10 01:07:55 +0400

    feat: type coercion null to number, null to string

---
+++ t1.GTU02.null.js
@@ -0,0 +1,7 @@
+null + undefined; //  NaN,  0 + NaN = NaN
+null + true; //  1          0 + 1 = 1
+null + false; //  0         0 + 0 = 0
+null + 3; //  3             0 + 3 = 3
+null + NaN; // NaN          0 + NaN = NaN
+null + "hello"; // "nullhello"  null + "string" = "nullstring"
+null + a + b + c + d + e + f; // Reff. error,
