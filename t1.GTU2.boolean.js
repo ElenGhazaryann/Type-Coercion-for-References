12494f2dd3176a71983cbe1421db2b657d43c2d2
Elen <elen.ghazaryan6@gmail.com>
2021-12-10 01:15:45 +0400

    feat: coercion booleans to number and to string

---
+++ t1.GTU2.boolean.js
@@ -0,0 +1,9 @@
+true + false; // 1           1 + 0 = 1
+true + 2; // 3               1 + 2 = 3
+true + NaN; // NaN           1 + NaN = NaN
+true + "hello"; // "truehello"    true + "string" = "truestring"
+true + a + b + c + d + e + f; // Reff error
+false + 4; // 4
+false + NaN; // NaN
+false + "hello"; // 'falsehello'
+false + a + b + c + d + e + f; // Reff error
